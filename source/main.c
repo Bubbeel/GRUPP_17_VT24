f1311faa57f640819f8c29a9a336cb582347a461
Erik Olsson <eolsson4@kth.se>
2024-04-11 13:29:21 +0200

    Texture Flagga i main

--- source/main.c
+++ source/main.c
@@ -8,6 +8,9 @@
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 400
 
+#define FLAG_FRAME_RATE 10
+#define PLAYER_FRAME_RATE 60
+
 int main(int argc, char** argv) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Error: %s\n", SDL_GetError());
@@ -31,8 +34,9 @@ int main(int argc, char** argv) {
 
     SDL_Surface* pSurface1 = IMG_Load("resources/player1.png");
     SDL_Surface* pSurface2 = IMG_Load("resources/player2.png");
+    SDL_Surface* pSurfaceFlag = IMG_Load("resources/spritesheet.png");
 
-    if (!pSurface1 || !pSurface2) {
+    if (!pSurface1 || !pSurface2 || !pSurfaceFlag) {
         printf("Error: %s\n", SDL_GetError());
         SDL_DestroyRenderer(pRenderer);
         SDL_DestroyWindow(pWindow);
@@ -42,10 +46,13 @@ int main(int argc, char** argv) {
 
     SDL_Texture* pTexture1 = SDL_CreateTextureFromSurface(pRenderer, pSurface1);
     SDL_Texture* pTexture2 = SDL_CreateTextureFromSurface(pRenderer, pSurface2);
+    SDL_Texture* pTextureFlag = SDL_CreateTextureFromSurface(pRenderer, pSurfaceFlag);
     SDL_FreeSurface(pSurface1);
     SDL_FreeSurface(pSurface2);
+    SDL_FreeSurface(pSurfaceFlag);
 
-    if (!pTexture1 || !pTexture2) {
+
+    if (!pTexture1 || !pTexture2 || !pTextureFlag) {
         printf("Error: %s\n", SDL_GetError());
         SDL_DestroyRenderer(pRenderer);
         SDL_DestroyWindow(pWindow);
@@ -55,14 +62,17 @@ int main(int argc, char** argv) {
 
     SDL_Rect playerRect1;
     SDL_Rect playerRect2;
+    SDL_Rect flagRect;
     SDL_QueryTexture(pTexture1, NULL, NULL, &playerRect1.w, &playerRect1.h);
     SDL_QueryTexture(pTexture2, NULL, NULL, &playerRect2.w, &playerRect2.h);
+    SDL_QueryTexture(pTextureFlag, NULL, NULL, &flagRect.w, &flagRect.h);
 
     //storlek
     playerRect1.w /= 20;
     playerRect1.h /= 20;
     playerRect2.w /= 20;
     playerRect2.h /= 20;
+    flagRect.w /= 5;
 
     // Startpositioner:
 
@@ -74,10 +84,17 @@ int main(int argc, char** argv) {
     float player2X = WINDOW_WIDTH - playerRect2.w; 
     float player2Y = WINDOW_HEIGHT - playerRect2.h;
 
+    // flagga i mitten
+    flagRect.x = (WINDOW_WIDTH - flagRect.w) / 2;
+    flagRect.y = (WINDOW_HEIGHT - flagRect.h) / 2;
+
     float player1VelocityX = 0;
     float player1VelocityY = 0;
     float player2VelocityX = 0;
     float player2VelocityY = 0;
+
+    //flaggans framerate
+    int flagFrame = 0;
 
     bool closeWindow = false;
     bool up1, down1, left1, right1;
@@ -153,6 +170,16 @@ int main(int argc, char** argv) {
             }
         }
 
+
+        SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
+        SDL_RenderClear(pRenderer);
+
+        SDL_Rect srcRect = { flagFrame * flagRect.w, 0, flagRect.w, flagRect.h };
+        SDL_RenderCopy(pRenderer, pTextureFlag, &srcRect, &flagRect);
+        SDL_RenderPresent(pRenderer);
+
+        flagFrame = (flagFrame + 1) % 5;
+
         // Spelare 1 rörelse
         player1VelocityX = player1VelocityY = 0;
         if (up1 && !down1) player1VelocityY = -SPEED;
@@ -183,17 +210,21 @@ int main(int argc, char** argv) {
         playerRect2.x = player2X;
         playerRect2.y = player2Y;
 
-        SDL_RenderClear(pRenderer);
+
         SDL_RenderCopy(pRenderer, pTexture1, NULL, &playerRect1);
         SDL_RenderCopy(pRenderer, pTexture2, NULL, &playerRect2);
         SDL_RenderPresent(pRenderer);
-        SDL_Delay(1000 / 60); // 60 frames/s
+        SDL_Delay(1000 / PLAYER_FRAME_RATE); // 60 frames/s
+
+
     }
 
     SDL_DestroyTexture(pTexture1);
     SDL_DestroyTexture(pTexture2);
+    SDL_DestroyTexture(pTextureFlag); // flagga
     SDL_DestroyRenderer(pRenderer);
     SDL_DestroyWindow(pWindow);
+
 
     SDL_Quit();
     return 0;
